995bb55ab47ec84cd8f21c4f3310d9dfc157e7c2
Luke Devlin <ldevlin559@gmail.com>
2024-08-26 12:34:04 +0100

    JobRole Model

--- src/main/java/org/example/models/JobRole.java
+++ src/main/java/org/example/models/JobRole.java
@@ -6,5 +6,69 @@ public class JobRole {
     private int jobRoleId;
     private String roleName;
     private String location;
+    private int capabilityId;
+    private int bandId;
     private Date closingDate;
+
+    public JobRole(final int jobRoleId,
+                   final String roleName,
+                   final String location,
+                   final int capabilityId,
+                   final int bandId,
+                   final Date closingDate) {
+        this.jobRoleId = jobRoleId;
+        this.roleName = roleName;
+        this.location = location;
+        this.capabilityId = capabilityId;
+        this.bandId = bandId;
+        this.closingDate = closingDate;
+    }
+
+    public int getJobRoleId() {
+        return jobRoleId;
+    }
+
+    public void setJobRoleId(final int jobRoleId) {
+        this.jobRoleId = jobRoleId;
+    }
+
+    public Date getClosingDate() {
+        return closingDate;
+    }
+
+    public void setClosingDate(final Date closingDate) {
+        this.closingDate = closingDate;
+    }
+
+    public int getCapabilityId() {
+        return capabilityId;
+    }
+
+    public void setCapabilityId(final int capabilityId) {
+        this.capabilityId = capabilityId;
+    }
+
+    public int getBandId() {
+        return bandId;
+    }
+
+    public void setBandId(final int bandId) {
+        this.bandId = bandId;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(final String location) {
+        this.location = location;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(final String roleName) {
+        this.roleName = roleName;
+    }
 }
