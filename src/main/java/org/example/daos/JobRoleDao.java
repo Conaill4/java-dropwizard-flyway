21f4ce359d81528959979fe0e748c58543e9c342
Luke Devlin <ldevlin559@gmail.com>
2024-08-26 15:45:22 +0100

    Fix

--- src/main/java/org/example/daos/JobRoleDao.java
+++ src/main/java/org/example/daos/JobRoleDao.java
@@ -17,9 +17,9 @@ public class JobRoleDao {
             ResultSet resultSet = statement.executeQuery(
                     "SELECT jobRoleId, roleName, location,"
                     + "capability.capabilityName, band.bandName, "
-                    + "closingDate FROM job-roles"
-                     + "JOIN capability using(capability.capabilityId)"
-                     + "JOIN band using(band.bandId)");
+                    + "closingDate FROM `job-roles`"
+                     + "JOIN capability using(Capability.capabilityId)"
+                     + "JOIN band using(Band.bandId)");
             while (resultSet.next()) {
                 JobRole jobRole = new JobRole(
                         resultSet.getInt("jobRoleId"),
