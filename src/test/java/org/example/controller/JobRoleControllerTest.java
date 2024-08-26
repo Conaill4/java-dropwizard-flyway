2abc67153a8107bca8853b564f4a98c57964ca31
JohnnyWork718 <johnny----18@hotmail.com>
2024-08-26 14:48:04 +0100

    Added imports and dependencies for tests

--- src/test/java/org/example/controller/JobRoleControllerTest.java
+++ src/test/java/org/example/controller/JobRoleControllerTest.java
@@ -1,4 +1,13 @@
 package org.example.controller;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import javax.ws.rs.core.Response;
+import java.sql.SQLException;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
 public class JobRoleControllerTest {
+
+
 }
--- src/test/java/org/example/service/JobRoleServiceTest.java
+++ src/test/java/org/example/service/JobRoleServiceTest.java
@@ -1,4 +1,15 @@
 package org.example.service;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class JobRoleServiceTest {
 }
