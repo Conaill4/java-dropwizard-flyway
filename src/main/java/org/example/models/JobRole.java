2e5bb3f08ed323967b279e09aef2c0d4e20cae39
Luke Devlin <ldevlin559@gmail.com>
2024-08-26 12:26:06 +0100

    Test

---
+++ src/main/java/org/example/models/JobRole.java
@@ -0,0 +1,10 @@
+package org.example.models;
+
+import java.util.Date;
+
+public class JobRole {
+    private int jobRoleId;
+    private String roleName;
+    private String location;
+    private Date closingDate;
+}
