7a8f41e4d6dd32d2acc6c1460caf4db834642fc8
Luke Devlin <ldevlin559@gmail.com>
2024-08-27 10:46:53 +0100

    tests

--- src/test/java/org/example/controller/JobRoleControllerTest.java
+++ src/test/java/org/example/controller/JobRoleControllerTest.java
@@ -1,7 +1,6 @@
 package org.example.controller;
 
 import org.example.controllers.JobRoleController;
-import org.example.models.JobRole;
 import org.example.models.JobRoleResponse;
 import org.example.services.JobRoleService;
 import org.junit.jupiter.api.Test;
@@ -9,12 +8,10 @@ import org.mockito.Mockito;
 import javax.ws.rs.core.Response;
 import java.sql.Date;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 class JobRoleControllerTest {
