19ab9e71e79966e2d567467eeb83b93a7e090403
TaylorB <taylor.brogan@kainos.com>
2024-08-30 13:57:48 +0100

    Mapper Test Fix 2

--- src/test/java/org/example/mapper/JobRoleMapperTest.java
+++ src/test/java/org/example/mapper/JobRoleMapperTest.java
@@ -1,20 +1,16 @@
 package org.example.mapper;
 
-import org.example.daos.JobRoleDao;
 import org.example.mappers.JobRoleMapper;
 import org.example.models.JobRole;
 import org.example.models.JobRoleResponse;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import java.sql.Date;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
 
 public class JobRoleMapperTest {
 
