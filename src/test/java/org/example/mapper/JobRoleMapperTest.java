82ac92f660908fa4b726283caac0c9a272ae3fab
TaylorB <taylor.brogan@kainos.com>
2024-08-30 13:51:51 +0100

    Mapper Test Fix

--- src/test/java/org/example/mapper/JobRoleMapperTest.java
+++ src/test/java/org/example/mapper/JobRoleMapperTest.java
@@ -7,26 +7,47 @@ import org.example.models.JobRoleResponse;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.sql.Date;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 public class JobRoleMapperTest {
 
-    JobRoleDao jobRoleDao = Mockito.mock(JobRoleDao.class);
-    JobRoleMapper jobRoleMapper = Mockito.mock(JobRoleMapper.class);
+    JobRole jobRole1 = new JobRole(
+            1,
+            "Manager",
+            "Derry",
+            "Intern",
+            "Grade 1 -£20,000 - 25,000",
+            Date.valueOf("2024-12-30"));
 
     @Test
     void getJobRoleMapper_shouldReturnJobRoles() throws SQLException{
 
-        List<JobRole> expectedJobRoles = new ArrayList<>();
-        Mockito.when(jobRoleDao.getJobRoles()).thenReturn(expectedJobRoles);
+        JobRoleMapper jobRoleMapper = new JobRoleMapper();
+        List<JobRole> jobRoles = new ArrayList<>();
+        jobRoles.add(jobRole1);
 
-        List<JobRoleResponse> JobRole = jobRoleMapper
-                .mapOrderListToJobRoleResponseList(jobRoleDao.getJobRoles());
+        List<JobRoleResponse> jobRoleResponse = jobRoleMapper
+                .mapOrderListToJobRoleResponseList(jobRoles);
 
-        assertEquals(expectedJobRoles, JobRole);
+        assertEquals(1, jobRoleResponse.size());
+        assertEquals(jobRole1.getJobRoleId(),
+                jobRoleResponse.get(0).getJobRoleId());
+        assertEquals(jobRole1.getRoleName(),
+                jobRoleResponse.get(0).getRoleName());
+        assertEquals(jobRole1.getLocation(),
+                jobRoleResponse.get(0).getLocation());
+        assertEquals(jobRole1.getCapabilityName(),
+                jobRoleResponse.get(0).getCapabilityName());
+        assertEquals(jobRole1.getBandName(),
+                jobRoleResponse.get(0).getBandName());
+        assertEquals(jobRole1.getClosingDate(),
+                jobRoleResponse.get(0).getClosingDate());
     }
 }
