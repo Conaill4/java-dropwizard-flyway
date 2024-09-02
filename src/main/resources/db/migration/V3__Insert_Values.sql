53a3ecfe1a519653ca70d42e6b297d70ef4c8687
Luke Devlin <ldevlin559@gmail.com>
2024-09-02 12:46:48 +0100

    Changed rows in db

--- src/main/resources/db/migration/V3__Insert_Values.sql
+++ src/main/resources/db/migration/V3__Insert_Values.sql
@@ -1,16 +1,17 @@
-INSERT INTO Band (bandName) VALUES('Grade 1 -£20,000 - 25,000');
-INSERT INTO Band (bandName) VALUES('Grade 2 - £25,001 -  30,000');
-INSERT INTO Band (bandName) VALUES('Grade 3 -£30,001 - 40,000');
-INSERT INTO Band (bandName) VALUES('Grade 4- £40,001 - 50,000');
-INSERT INTO Band (bandName) VALUES('Grade 5 -£50,001+');
+INSERT INTO Band (bandName) VALUES('Principal');
+INSERT INTO Band (bandName) VALUES('Manager');
+INSERT INTO Band (bandName) VALUES('Consultant');
+INSERT INTO Band (bandName) VALUES('Senior Associate');
+INSERT INTO Band (bandName) VALUES('Trainee');
+INSERT INTO Band (bandName) VALUES('Apprentice');
 
-INSERT INTO Capability (capabilityName) VALUES ('Intern');
-INSERT INTO Capability (capabilityName) VALUES ('Graduate');
-INSERT INTO Capability (capabilityName) VALUES ('Junior');
-INSERT INTO Capability (capabilityName) VALUES ('Mid-Level');
-INSERT INTO Capability (capabilityName) VALUES ('Senior');
-INSERT INTO Capability (capabilityName) VALUES ('Lead');
+INSERT INTO Capability (capabilityName) VALUES ('Engineering');
+INSERT INTO Capability (capabilityName) VALUES ('Data');
+INSERT INTO Capability (capabilityName) VALUES ('People');
+INSERT INTO Capability (capabilityName) VALUES ('AI');
+INSERT INTO Capability (capabilityName) VALUES ('UX');
+INSERT INTO Capability (capabilityName) VALUES ('Testing');
 
 
-INSERT INTO `job-roles` (roleName,location,capabilityId,bandId,closingDate) VALUES("Software Engineer","Derry",'1','1','2024-12-25');
-INSERT INTO `job-roles` (roleName,location,capabilityId,bandId,closingDate) VALUES("Tester","Derry",'2','2','2024-12-26');
+INSERT INTO `job-roles` (roleName,location,capabilityId,bandId,closingDate) VALUES("Software Engineer","Derry",'1','5','2024-12-25');
+INSERT INTO `job-roles` (roleName,location,capabilityId,bandId,closingDate) VALUES("Tester","Derry",'6','2','2024-12-26');
