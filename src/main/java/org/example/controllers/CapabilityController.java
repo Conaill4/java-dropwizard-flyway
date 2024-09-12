82c335d9b4810f235e7fa708efb829e98d0b68b9
Amara Uzoma <amara.uzoma@kainos.com>
2024-09-12 16:56:02 +0100

    Conflicts

--- src/main/java/org/example/controllers/CapabilityController.java
+++ src/main/java/org/example/controllers/CapabilityController.java
@@ -1,10 +1,8 @@
 package org.example.controllers;
 
 import io.swagger.annotations.Api;
-import org.example.models.UserRole;
 import org.example.services.CapabilityService;
 
-import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -13,7 +11,6 @@ import javax.ws.rs.core.Response;
 import java.sql.SQLException;
 
 @Api("JobRole API")
-@RolesAllowed({UserRole.ADMIN})
 @Path("/api/Capability")
 public class CapabilityController {
 
