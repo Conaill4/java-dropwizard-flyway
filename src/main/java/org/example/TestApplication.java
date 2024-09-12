9098d7740b746e602428b17e40448c473327adcb
Conaill <conaill2004@outlook.com>
2024-09-12 12:50:23 -0700

    band and capability endpoints

--- src/main/java/org/example/TestApplication.java
+++ src/main/java/org/example/TestApplication.java
@@ -12,14 +12,20 @@ import io.jsonwebtoken.Jwts;
 import org.example.auth.JwtAuthenticator;
 import org.example.auth.RoleAuthorisor;
 import org.example.controllers.AuthController;
+import org.example.controllers.BandController;
+import org.example.controllers.CapabilityController;
 import org.example.controllers.JobRoleController;
 import org.example.controllers.TestController;
 import org.example.daos.AuthDao;
+import org.example.daos.BandDao;
+import org.example.daos.CapabilityDao;
 import org.example.daos.JobRoleDao;
 import org.example.daos.TestDao;
 import org.example.mappers.JobRoleMapper;
 import org.example.models.JwtToken;
 import org.example.services.AuthService;
+import org.example.services.BandService;
+import org.example.services.CapabilityService;
 import org.example.services.JobRoleService;
 import org.example.services.TestService;
 import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
@@ -64,11 +70,20 @@ public class TestApplication extends Application<TestConfiguration> {
         environment.jersey().register(new TestController(
                 new TestService(new TestDao())));
         environment.jersey().register((new JobRoleController(
-               new JobRoleService(new JobRoleDao(), new JobRoleMapper()),
+                new JobRoleService(new JobRoleDao(), new JobRoleMapper()),
                 new PaginationSanitiser()
-                )));
+        )));
         environment.jersey().register(new AuthController(
                 new AuthService(new AuthDao(), jwtKey, new AuthValidator())));
-    }
 
+        environment.jersey()
+                .register(new BandController(
+                        new BandService(
+                                new BandDao()
+                                )));
+        environment.jersey()
+                .register(new CapabilityController(
+                        new CapabilityService(
+                                new CapabilityDao())));
+    }
 }
